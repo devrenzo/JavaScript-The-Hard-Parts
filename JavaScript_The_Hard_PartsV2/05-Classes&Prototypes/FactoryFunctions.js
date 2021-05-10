155db34f4f40d84c40226fc71a5384a4d175c686
Renzo Barrios <w3renzo@gmail.com>
2021-05-09 21:17:09 -0300

    CSS Grid and Flexbox init

---
+++ JavaScript_The_Hard_PartsV2/05-Classes&Prototypes/FactoryFunctions.js
@@ -0,0 +1,16 @@
+function userCreator(name, score) {
+    const newUser = {};
+    newUser.name = name;
+    newUser.score = score;
+    newUser.increment = function () {
+        newUser.score++;
+    };
+    return newUser;
+}
+
+const user1 = userCreator('Renzo', 5);
+const user2 = userCreator('Pinta', 3);
+
+user1.increment();
+
+console.log(user1, user2);
