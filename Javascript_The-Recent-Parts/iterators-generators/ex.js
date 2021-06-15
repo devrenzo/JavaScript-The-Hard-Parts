bef9a16857094c634da53678edbaad33780e45c9
Renzo Barrios <w3renzo@gmail.com>
2021-06-15 14:18:20 -0300

    Iterators

--- Javascript_The-Recent-Parts/iterators-generators/ex.js
+++ Javascript_The-Recent-Parts/iterators-generators/ex.js
@@ -1,14 +1,24 @@
 var numbers = {
-	// ..
+
+    *[Symbol.iterator]({
+        min = 0,
+        max = 100,
+        step = 1
+    } = {}) {
+
+        for (let i = min; i <= max; i += step) {
+            yield i;
+        }
+    }
 };
 
 // should print 0..100 by 1s
 for (let num of numbers) {
-	console.log(num);
+    console.log(num);
 }
 
 // should print 6..30 by 4s
-console.log("My lucky numbers are: ____");
+console.log(`My lucky numbers are: ${[...numbers[Symbol.iterator]({min:6,max:30,step:4})]}`);
 
 // Hint:
 //     [...numbers[Symbol.iterator]( ?? )]
